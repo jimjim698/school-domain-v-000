f540af04f6bdee704de34433bf903e021ac13774
James Lotruglio <jimlotruglio@yahoo.com>
2018-03-26 17:25:05 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -14,8 +14,12 @@ end
 def add_student(name, grade)
   @name = name
   @grade = grade
+  if @roster.include?(@grade)
+    @roster[@grade]<< @name
+  else
   @roster[@grade]=[]
   @roster[@grade]<< @name
+end
 
 end
 
